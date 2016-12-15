2aabc7d1b6cc210525cc75cf9663b5cedd1d4b22
José Sánchez <joseynene@hotmail.com>
2016-12-16 00:28:12 +0100

    Solucion Broken White Path.Introducción de Likes en los Productos

--- app/components/product-detail/product-detail.component.ts
+++ app/components/product-detail/product-detail.component.ts
@@ -6,6 +6,9 @@ import { ConfirmationService } from "primeng/primeng";
 
 import { Product } from "../../models/product";
 import { ProductService } from "../../services/product.service";
+import getPrototypeOf = Reflect.getPrototypeOf;
+
+
 
 @Component({
     templateUrl: "./app/components/product-detail/product-detail.component.html",
@@ -16,6 +19,9 @@ export class ProductDetailComponent implements OnDestroy, OnInit {
     private _product: Product;
     private _productSubscription: Subscription;
 
+    private _productLikeLocalStorageKey: string;
+    private _productLiked: boolean = false;
+
     constructor(
         private _productService: ProductService,
         private _route: ActivatedRoute,
@@ -23,7 +29,12 @@ export class ProductDetailComponent implements OnDestroy, OnInit {
         private _confirmationService: ConfirmationService) { }
 
     ngOnInit(): void {
-        this._route.data.forEach((data: { product: Product }) => this._product = data.product);
+        // this._route.data.forEach((data: { product: Product }) => this._product = data.product);
+        this._route.data.forEach((data: { product: Product }) => {
+            this._product = data.product;
+            this._productLikeLocalStorageKey = `whatapop-product-likes-${this._product.id}`;
+            this._productLiked = this._getProductLike();
+        });
         window.scrollTo(0, 0);
     }
 
@@ -60,5 +71,30 @@ export class ProductDetailComponent implements OnDestroy, OnInit {
 
     goBack(): void {
         window.history.back();
+    }
+
+    /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+    | Broken White Path                                                |
+    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+    | Funciones manejadoras de los Like de un Producto                 |
+    |~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+    // Función que recupera si el producto tiene un Like
+    private _getProductLike(): boolean {
+        if (typeof(Storage) !== undefined) {
+            return localStorage.getItem(this._productLikeLocalStorageKey) === "true";
+        }
+    }
+
+    // Función que establece un Like al Producto
+    private _setProductLike(like: boolean): void {
+        if (typeof(Storage) !== undefined) {
+            localStorage.setItem(this._productLikeLocalStorageKey, (like ? "true" : "false"));
+        }
+    }
+
+    // Función manejadora de Likes. Activa o desactiva el Like del Producto
+    likingProduct(): void {
+        this._productLiked = !this._productLiked;
+        this._setProductLike(this._productLiked);
     }
 }
